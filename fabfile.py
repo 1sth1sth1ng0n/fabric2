4eeb7132dd2b8a02f8e63aec3eb92cef35087d26
Olly <olly811@msn.com>
2023-05-12 08:07:36 -0400

    add sudo

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,14 @@
-from fabric import task, Connection
+from fabric import task, Connection, Config
+import os
 
 """ check if tomcat is running on remote host 
 
-    example: fab2 -H '[USER@HOSTNAME]' checkTomcat
+    example: fab2 -H '[USER@HOSTNAME]' check-tomcat
 
 """
 
 @task
-def checkTomcat(ctx):
+def check_tomcat(ctx):
 
     """ acquisition and release - no need to c.close """
     with Connection(
@@ -19,5 +20,30 @@ def checkTomcat(ctx):
     ) as c:
 
         c.run('systemctl is-active jamf.tomcat8.service')
+
+
+""" restart tomcat on remote host 
+
+    example: fab2 -H '[USER@HOSTNAME]' restart-tomcat
+
+"""
+
+@task
+def restart_tomcat(ctx):
+    
+    cred = os.environ.get("ADM_USER_PASS")
+    config = Config(overrides={'sudo': {'password': cred}})
+
+    """ acquisition and release, no need to c.close """
+    with Connection(
+        ctx.host,
+        config=config,
+        connect_timeout=4,
+        connect_kwargs={
+            "key_filename": "[SSH_KEY_PATH]"
+    }
+    ) as c:
+        
+        c.sudo('systemctl restart jamf.tomcat8.service', hide='stderr')
 
 
